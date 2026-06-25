858ee793c1e74e5891b222cce30a0b2056bc9c12
Bihrys <3200769991@qq.com>
2026-06-25 22:56:40 +0800

    1

--- crates/launcher-core/src/lib.rs
+++ crates/launcher-core/src/lib.rs
@@ -24,10 +24,6 @@ pub use auth::{
     refresh_account, save_account, save_auth_servers, select_account, select_account_identifier,
     selected_account, upload_account_skin,
 };
-pub use download_center::{
-    DownloadCatalogService, DownloadCenterTaskKind, DownloadService, DownloadSourceKind,
-    DownloadTab, GameInstallerService, LoaderKind,
-};
 
 pub use game_download::{
     InstallResult, fetch_download_catalog_json, install_game_version,
